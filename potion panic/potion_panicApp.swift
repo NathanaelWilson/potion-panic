a59300f8267deb18283edf207a345db9a052a44f
NathanaelWilson <nathanaelwilsonangelo@gmail.com>
2026-03-19 18:35:43 +0700

    Initial Commit

---
+++ potion panic/potion_panicApp.swift
@@ -0,0 +1,17 @@
+//
+//  potion_panicApp.swift
+//  potion panic
+//
+//  Created by Nathanael Wilson Angelo on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct potion_panicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
